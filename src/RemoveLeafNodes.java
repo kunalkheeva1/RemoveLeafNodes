801becc756ac60277240a15be5ece71ff268ff90
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-21 19:29:29 -0700

    coded a method to delete the leaf nodes with a target values, anyhow it works with the recursion, but I am still confusing why are we storing the values of recursion in root.left and root.right

--- src/RemoveLeafNodes.java
+++ src/RemoveLeafNodes.java
@@ -10,7 +10,17 @@ public class RemoveLeafNodes {
             this.right = right;
         }
     }
+    static Node removeLeafNodes(Node root, int target){
+        if(root == null) return null;
 
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+
+        if(root.left == null && root.right == null && root.val == target ){
+            return null;
+        }
+        return root;
+    }
 
 
     public static void main(String[] args) {
