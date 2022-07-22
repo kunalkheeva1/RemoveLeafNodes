a6618ed3f4beb680c2200a14016264e3312a6721
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-21 19:23:31 -0700

    coded the node class

--- src/RemoveLeafNodes.java
+++ src/RemoveLeafNodes.java
@@ -1,5 +1,19 @@
 public class RemoveLeafNodes {
+
+    class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val, Node left, Node right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        
+
     }
 }
