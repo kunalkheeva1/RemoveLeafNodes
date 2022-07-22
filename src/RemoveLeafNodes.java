d7935dc63169b993089ddefc62502bdf4b7d2c0d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-21 19:31:10 -0700

    added the comments for better understanding

--- src/RemoveLeafNodes.java
+++ src/RemoveLeafNodes.java
@@ -1,5 +1,5 @@
 public class RemoveLeafNodes {
-
+    //node class
     class Node{
         int val;
         Node left;
@@ -10,12 +10,16 @@ public class RemoveLeafNodes {
             this.right = right;
         }
     }
+
+
     static Node removeLeafNodes(Node root, int target){
         if(root == null) return null;
 
+        //movement of left and right with target
         root.left = removeLeafNodes(root.left, target);
         root.right = removeLeafNodes(root.right, target);
 
+        // if left and right and value is reached then return null there
         if(root.left == null && root.right == null && root.val == target ){
             return null;
         }
